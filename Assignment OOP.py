de4b4bc55af11368b4c367f07e562af9c29b7f81
AnujSharma100 <117298925+AnujSharma100@users.noreply.github.com>
2022-12-13 23:58:59 +0530

    Add files via upload

---
+++ Assignment OOP.py
@@ -0,0 +1,67 @@
+challenge 1
+class Point:
+    def __init__(self,x,y,z):
+        self.x= x
+        self.y= y
+        self.z= z
+
+    def sqsum(self):
+        return(self.x*self.x+self.y*self.y+self.z*self.z)
+x=int(input('enter first num'))
+y=int(input('enter second num'))
+z=int(input('enter third num'))
+sqno=Point(x,y,z)
+print(sqno.sqsum())
+
+
+challenge 2
+class calculator():
+    def __init__(self,num1,num2):
+        self.num1=num1
+        self.num2=num2
+    def add(self):
+        return self.num1+self.num2
+    def sub(self):
+        return self.num2-self.num1
+    def mult(self):
+        return self.num1*self.num2
+    def div(self):
+        return self.num2/self.num1
+num1=int(input('enter first no.='))
+num2=int(input('enter second no.='))
+obj=calculator(num1,num2)
+print(obj.add())
+print(obj.sub())
+print(obj.mult())
+print(obj.div())
+
+
+challenge 3
+class Student:
+    name=None
+    rollNumber=None
+
+    def setName (self,name):
+        self.name=name
+
+    def getName (self):
+        return self.name
+
+    def setRollNumber(self, rollNumber):
+        self.rollNumber=rollNumber
+    
+    def getRollNumber(self):
+        return self.rollNumber
+
+    syntax1=Student()
+    syntax1.setName('anshul')
+    syntax1.setRollNumber(45)
+    print(syntax1.getRollNumber())
+
+
+#challenge 4
+class account:
+    def __init__(self,title):
+
+        
+
